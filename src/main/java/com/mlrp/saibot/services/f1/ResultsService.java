785cc04fded7445d6eb36695dc007ce7787ad6fc
Luke Prananta <lukeprananta@gmail.com>
2023-06-19 19:49:38 +0200

    Introduce delta times per qualifying session

--- src/main/java/com/mlrp/saibot/services/f1/ResultsService.java
+++ src/main/java/com/mlrp/saibot/services/f1/ResultsService.java
@@ -1,15 +1,16 @@
 package com.mlrp.saibot.services.f1;
 
-import static com.mlrp.saibot.services.f1.ResultsService.SessionType.RACE;
-
 import com.mlrp.saibot.clients.ErgastClient;
 import com.mlrp.saibot.clients.domain.ergast.Race;
 import com.mlrp.saibot.clients.domain.ergast.Result;
 import java.time.Clock;
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -17,26 +18,24 @@ import reactor.core.publisher.Mono;
 public class ResultsService {
   private final ErgastClient client;
   private final ScheduleService scheduleService;
-  private final Clock clock;
 
   public ResultsService(ErgastClient client, ScheduleService scheduleService, Clock clock) {
     this.client = client;
     this.scheduleService = scheduleService;
-    this.clock = clock;
   }
 
   public Mono<Results> getLastRaceResults() {
     return client
         .fetchLastRaceResults()
         .filter(race -> race.results().isPresent())
-        .map(race -> new Results(race.raceName(), RACE, race.results().orElseThrow()));
+        .map(race -> new Results(race.raceName(), race.results().orElseThrow()));
   }
 
   public Mono<Results> getLastResult() {
     return client
         .fetchLastRaceResults()
         .filter(race -> race.results().isPresent())
-        .map(race -> new Results(race.raceName(), RACE, race.results().orElseThrow()));
+        .map(race -> new Results(race.raceName(), race.results().orElseThrow()));
   }
 
   public Mono<QualifyingResults> getLastQualifyingResults() {
@@ -56,34 +55,70 @@ public class ResultsService {
                     new QualifyingResult(
                         result.driver().familyName(),
                         result.position(),
-                        result.q1(),
-                        result.q2(),
-                        result.q3()))
+                        getQualifyingTimes(result)))
             .toList());
   }
 
-  public record Results(String raceName, SessionType sessionType, List<Result> results) {}
+  private static Map<QualifyingSession, Duration> getQualifyingTimes(
+      com.mlrp.saibot.clients.domain.ergast.QualifyingResult result) {
+    Map<QualifyingSession, Duration> map = new HashMap<>();
+    result.q1().ifPresent(time -> map.put(QualifyingSession.Q1, parseDuration(time)));
+    result.q2().ifPresent(time -> map.put(QualifyingSession.Q2, parseDuration(time)));
+    result.q3().ifPresent(time -> map.put(QualifyingSession.Q3, parseDuration(time)));
+    return map;
+  }
+
+  private static Duration parseDuration(String time) {
+    Pattern regex = Pattern.compile("^(\\d+):(\\d+)\\.(\\d+)$");
+    Matcher matcher = regex.matcher(time);
+    if (matcher.matches()) {
+      int minutes = Integer.parseInt(matcher.group(1));
+      int seconds = Integer.parseInt(matcher.group(2));
+      int milliseconds = Integer.parseInt(matcher.group(3));
+      return Duration.ofMillis((minutes * 60L + seconds) * 1000L + milliseconds);
+    }
+    return Duration.ZERO;
+  }
+
+  public record Results(String raceName, List<Result> results) {}
 
   public record RaceResult(String familyName, int position, int points, String time) {}
 
-  public record QualifyingResults(String raceName, List<QualifyingResult> results) {}
-
-  public record QualifyingResult(
-      String familyName,
-      int position,
-      Optional<String> q1,
-      Optional<String> q2,
-      Optional<String> q3) {
-    public String getTime() {
-      return Stream.of(q3.stream(), q2.stream(), q1.stream())
-          .flatMap(Function.identity())
-          .findFirst()
-          .orElseThrow();
+  public record QualifyingResults(String raceName, List<QualifyingResult> results) {
+    public List<QualifyingSessionResult> getQualifyingSessionResults(
+        QualifyingSession qualifyingSession) {
+      return results.stream()
+          .filter(
+              qualifyingResult -> qualifyingResult.qualifyingTimes().containsKey(qualifyingSession))
+          .map(
+              qualifyingResult ->
+                  new QualifyingSessionResult(
+                      qualifyingResult.familyName(),
+                      qualifyingResult.qualifyingTimes().get(qualifyingSession)))
+          .sorted(Comparator.comparing(QualifyingSessionResult::time))
+          .toList();
     }
   }
 
-  public enum SessionType {
-    RACE,
-    QUALIFYING
+  public record QualifyingResult(
+      String familyName, int position, Map<QualifyingSession, Duration> qualifyingTimes) {}
+
+  public record QualifyingSessionResult(String familyName, Duration time) {
+    public String getFormattedTime() {
+      long minutes = time.toMinutes();
+      long seconds = time.minusMinutes(minutes).getSeconds();
+      long milliseconds = time.toMillis() % 1000;
+      return String.format("%d:%02d.%03d", minutes, seconds, milliseconds);
+    }
+
+    public String getDelta(Duration topTime) {
+      return "+" + ((time.toNanos() - topTime.toNanos()) / 1e9);
+    }
+  }
+
+  public enum QualifyingSession {
+    Q1,
+    Q2,
+    Q3
   }
 }
